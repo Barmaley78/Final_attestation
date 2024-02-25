772d2f618fb2188b1db07b2b5c01b14a19f05658
Sergey <barmaley78@mail.ru>
2024-02-25 18:27:03 +0700

    blog page added

---
+++ blog.js
@@ -0,0 +1,49 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        blogs: [
+            {
+                id: 1,
+                image: './images/banner_blog1.jpg',
+                buttonText: 'Kitchen Design',
+                headingText: 'Let’s Get Solution For Building Construction Work',
+                descriptionText: '26 December,2022'
+            },
+            {
+                id: 2,
+                image: './images/banner_blog2.jpg',
+                buttonText: 'Living Design',
+                headingText: 'Low Cost Latest Invented Interior Designing Ideas.',
+                descriptionText: '22 December,2022'
+            },
+            {
+                id: 3,
+                image: './images/banner_blog3.jpg',
+                buttonText: 'Interior Design',
+                headingText: 'Best For Any Office & Business Interior Solution',
+                descriptionText: '25 December,2022'
+            },
+            {
+                id: 4,
+                image: './images/banner_blog4.jpg',
+                buttonText: 'Kitchen Design',
+                headingText: 'Let’s Get Solution For Building Construction Work',
+                descriptionText: '26 December,2022'
+            },
+            {
+                id: 5,
+                image: './images/banner_blog5.jpg',
+                buttonText: 'Living Design',
+                headingText: 'Low Cost Latest Invented Interior Designing Ideas.',
+                descriptionText: '22 December,2022'
+            },
+            {
+                id: 6,
+                image: './images/banner_blog6.jpg',
+                buttonText: 'Interior Design',
+                headingText: 'Best For Any Office & Business Interior Solution',
+                descriptionText: '25 December,2022'
+            }
+        ]
+    }
+});
